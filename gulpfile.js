6180a4576ba7198f85751f57adc67abb3b429977
Dmytro Vertii <dmytro.vertii@tx.local>
2017-05-22 18:21:30 +0300

    update styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,20 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('public/stylesheets'));
 });
 
-gulp.task('watch', function() {
+/*gulp.task('watch', function() {
   gulp.watch('public/stylesheets/*.scss', ['sass']);
+});*/
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch('public/stylesheets/**/*.scss', ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
 
 gulp.task('default', ['sass', 'watch']);
