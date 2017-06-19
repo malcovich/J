088890d3acb2d7bdf4582c2dfda305d5a2659953
Dima Vertii <dmitriy.vertiy@gmail.com>
2017-06-19 22:54:28 +0300

    add search

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var conatactDetailsController = require('./server/controllers/contact-controller
 var friendController = require('./server/controllers/friends-controller');
 var requestController = require('./server/controllers/requests-controller');
 var messagesController = require('./server/controllers/messages-controller');
+var searchController = require('./server/controllers/search-controller');
 
 mongoose.connect('mongodb://localhost:27017/profee');
 app.use(bodyParser.json());
@@ -25,7 +26,6 @@ app.get('/', function(req, res){
 });
 
 //Auther
-
 
 app.post('/api/user/signup', authenticationController.signup)
 app.post('/api/contact/signup', authenticationController.signupContact)
@@ -59,6 +59,8 @@ app.post('/api/requests/deleteRequest', requestController.deleteRequest);
 app.post('/api/requests/saveAnswer', requestController.saveAnswer);
 app.post('/api/requests/getAnswer', requestController.getAnswer);
 app.post('/api/requests/getAllAnswers', requestController.getAllAnswers);
+
+app.post('/api/search', searchController.search);
 
 app.listen('3000', function(){
 	console.log("Port")
--- server/datasets/users.js
+++ server/datasets/users.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('User', {
 	email: String,
 	password: String,
-	name: String,
+	name: {type:String,text: true},
 	role: String
 }); 
