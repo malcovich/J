932ed94ac6757ea18882dfc0ee0848fceef87f41
Dmytro Vertii <dmytro.vertii@tx.local>
2017-07-10 15:30:21 +0300

    update add frind api

--- public/controllers/feed.controller.js
+++ public/controllers/feed.controller.js
@@ -2,8 +2,7 @@ angular.module('MyApp')
   .controller('FeedCtrl', ['$scope', '$log',  '$http','AuthFactory', function($scope, $log, $http, AuthFactory) {
   		AuthFactory.me().then(function(res){
   			$scope.user = res.data.data;
-  			$http.post('/api/friend/listFriendsRequests',{userId: $scope.user._id}).then(function(res){
-  				console.log(res.data)
+  			$http.post('/api/friend/listFriendsRequests',{userId: $scope.user._id}).then(function(res,err){
   				$scope.listRequest = res.data;
   			})
 
--- server/controllers/friends-controller.js
+++ server/controllers/friends-controller.js
@@ -16,7 +16,9 @@ module.exports.list = function(req,res) {
 }
 
 module.exports.listFriendsRequests = function(req,res) {
-	Friend.find({$and: [  {useridaccept : req.param('userId')} , { accepted: false }, { deleted: false }]}).populate('useridinvite').populate('useridaccept').exec(function (err, result) { 
+	console.log('req.',req.param("userId"))
+	Friend.find({$and: [{useridaccept : req.param('userId')} , { accepted: false }, { deleted: false }]}).populate('useridinvite').populate('useridaccept').exec(function (err, result) { 
+		console.log(result)
     	res.json(result);
 	});
 }
@@ -25,22 +27,14 @@ module.exports.accept = function(req,res) {
     	res.json(result);
 	});
 }
-/*module.exports.list = function(req, res){
-	Friend.find({ friendId1 : req.param('userId')}, function (err, result) {
-		console.log(result)
-       res.json(result);
-    });
-}
-*/
-module.exports.item = function(req, res) {
 
+module.exports.item = function(req, res) {
 	User.find({_id: req.body.friendId}).exec(function (err, result) {
 		Contact.find({ userId: result[0]._id}).exec(function (err, contacts) {
 			Friend.find({$and: [ {$or: [ {useridinvite : req.body.friendId} , {useridaccept : req.body.friendId} ]} , { accepted: true } , { deleted: false } ]}).populate('useridinvite').populate('useridaccept').exec(function (err, friends) { 
 	    		res.json({'friend': result[0], 'contacts': contacts, 'friends': friends });
 			});
 		});
-
 	});
 }
 
@@ -50,25 +44,6 @@ module.exports.deleteFriend = function(req, res){
 	  	if (err) throw err;
 	});
 }
-
-
-/*module.exports.item = function(req, res){
-	Friend.find({ _id : req.param('id')}).populate('_idmy').populate('_idfriend').exec(function (err, result) {
-		Contact.find({ userId: result[0].friendId1}).exec(function (err, contacts) {
-			console.log(result)
-        	res.json({'friend': result, 'contacts':contacts });
-		});
-
-				Contact.find({ $or:[ {userId: result[0].useridinvite} , {userId: result[0].useridaccept} ]}).exec(function (err, contacts) {
-			console.log(result)
-        	res.json({'friend': result, 'contacts':contacts });
-		});
-
-.populate('useridinvite').populate('useridaccept').exec(function(err, result)
-
-
-    });
-}*/
 
 module.exports.login = function(req,res){
 	User.find(req.body,function(err,results){
